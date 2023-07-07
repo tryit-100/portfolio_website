8506b29d7259e4291fdd3577dc7c06a7a0df0388
tryit-100 <safwan.std21@gmail.com>
2023-07-07 12:21:46 +0600

    scroll animation is done

---
+++ js/animation.js
@@ -0,0 +1,12 @@
+// Scroll animation imported from ScrollReveal website
+
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 4000,
+    reset: true
+});
+
+sr.reveal('#greeting, #name,.section-title, .title-animation', { delay: 200, origin: 'top' });
+sr.reveal('#banner-title, .back-end, .version-control, .about-me-content, .projects-section', { delay: 200, origin: 'bottom' });
+sr.reveal('.back-end, .design, .img-slider, .primary-btn, .vertical-name', { delay: 200, origin: 'right' });
+sr.reveal('.underline-div, .front-end, .database, .location', { delay: 200, origin: 'left' });
